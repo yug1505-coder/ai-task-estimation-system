a46043642f1ec49e3c8f6b67254075ea9ef7ec3d
Yug Babbar <yugbabbar@gmail.com>
2026-03-15 15:31:20 +0530

    Initial project setup with backend server

---
+++ backend/server.js
@@ -0,0 +1,16 @@
+const express = require("express")
+const cors = require("cors")
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+app.get("/", (req,res)=>{
+    res.send("AI Task Estimation Server Running")
+
+})
+
+app.listen(3000, ()=>{
+    console.log("Server running on port 3000")
+})
